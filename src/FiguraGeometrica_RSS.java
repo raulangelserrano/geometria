1752d97484aea0039d4fc42a86696c744bbbf5f6
Raúl Serrano <raulangel.serranopruebas@gmail.com>
2018-05-13 15:59:27 +0200

    Version refactorizada

--- src/FiguraGeometrica_RSS.java
+++ src/FiguraGeometrica_RSS.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_RSS {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_RSS(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
