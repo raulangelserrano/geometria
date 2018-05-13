f8ee16d067ab036381b4b132b3341c52dda358d2
Raúl Serrano <raulangel.serranopruebas@gmail.com>
2018-05-13 22:24:01 +0200

    Commit "extra" con la modificación de la clase triangulo, ya que en el commit de nuevo método solo lo habiamos hecho de la clase Figura geometrica

--- src/Triangulo_RSS.java
+++ src/Triangulo_RSS.java
@@ -23,7 +23,5 @@ public class Triangulo_RSS extends FiguraGeometrica_RSS {
 		return Math.sqrt(sp * (sp-lado1) * (sp-lado2) * (sp-lado3));
 	}
 	
-	public double semiPerimetro() {
-		return this.perimetro() / 2;
-	}
+	
 }
